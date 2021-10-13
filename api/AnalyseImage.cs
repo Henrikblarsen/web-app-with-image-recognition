724051a6b40fbb206b9191c84d870131e7338492
Henrikblarsen <79907092+Henrikblarsen@users.noreply.github.com>
2021-10-13 20:22:52 +0200

    debug

--- api/AnalyseImage.cs
+++ api/AnalyseImage.cs
@@ -14,7 +14,8 @@ namespace Company.Function
     {
         [FunctionName("AnalyseImage")]
         public static async Task<IActionResult>  Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest
+            req,
             ILogger log)
         {
             
