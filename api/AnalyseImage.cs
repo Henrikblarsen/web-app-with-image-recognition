f0ac72a013e2fbf1d2048d2083dc5f9e2d8cfca4
Henrikblarsen <79907092+Henrikblarsen@users.noreply.github.com>
2021-10-13 20:18:36 +0200

    Debugging

--- api/AnalyseImage.cs
+++ api/AnalyseImage.cs
@@ -13,13 +13,12 @@ namespace Company.Function
     public static class AnalyseImage
     {
         [FunctionName("AnalyseImage")]
-        public static async Task<IActionResult> Run(
+        public static async Task<IActionResult>  Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             
-            var someObject = new {text = "This is the returned message"};
-
+            var someObject = new { text = "This is the returned message" };
             return new OkObjectResult(someObject);
 
         }
