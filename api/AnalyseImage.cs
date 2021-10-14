a3bd39a8e80cea6c843062a1c2f55eb0990104b8
Henrikblarsen <79907092+Henrikblarsen@users.noreply.github.com>
2021-10-14 20:42:20 +0200

    Analyze Image from function

--- api/AnalyseImage.cs
+++ api/AnalyseImage.cs
@@ -6,6 +6,10 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 
 namespace Company.Function
@@ -15,12 +19,57 @@ namespace Company.Function
         [FunctionName("AnalyseImage")]
         public static async Task<IActionResult>  Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest
-            req,
-            ILogger log)
+            req, ILogger log, System.Threading.ExecutionContext context)
         {
-            var someObject = new {text = "This is the returned message" };
+            var config = new ConfigurationBuilder()
+         .SetBasePath(context.FunctionAppDirectory)
+         .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+         .AddEnvironmentVariables()
+         .Build();
 
-            return new OkObjectResult("This is the returned message");
+        // Add your Computer Vision subscription key and endpoint
+        string subscriptionKey = config["ComputerVisionKey"];
+        string endpoint = "https://web-app-with-image-recognition-hbla.cognitiveservices.azure.com/";
+
+        string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+        string imageUrl = data?.imageUrl;
+
+        // Create a client
+        ComputerVisionClient client = Authenticate(endpoint, subscriptionKey);
+        var analysisResult = await
+        // Analyze an image to get features and other properties.
+        AnalyzeImageUrl(client, imageUrl);
+
+            return new OkObjectResult(analysisResult);
         }
+
+        /*AUTHENTICATE - Creates a Computer Vision client used by each example.*/
+        public static ComputerVisionClient Authenticate(string endpoint, string key)
+        {
+            ComputerVisionClient client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
+            { Endpoint = endpoint };
+            return client;
+        }
+             
+            /* ANALYZE IMAGE - URL IMAGE / Analyze URL image. Extracts captions, categories, tags, objects, faces, racy/adult/gory content,
+            * brands, celebrities, landmarks, color scheme, and image types.*/
+            
+            public static async Task<ImageAnalysis> AnalyzeImageUrl(ComputerVisionClient client, string imageUrl)
+            {
+
+                List<VisualFeatureTypes?> features = new List<VisualFeatureTypes?>()
+                {
+                    VisualFeatureTypes.Categories, VisualFeatureTypes.Description,
+                    VisualFeatureTypes.Faces, VisualFeatureTypes.ImageType,
+                    VisualFeatureTypes.Tags, VisualFeatureTypes.Adult,
+                    VisualFeatureTypes.Color, VisualFeatureTypes.Brands,
+                    VisualFeatureTypes.Objects
+                };
+                // Analyze the URL image 
+                ImageAnalysis results = await client.AnalyzeImageAsync(imageUrl, visualFeatures: features);
+                return results;
+            }
     }
 }
